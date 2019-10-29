06ecfa7aed6ad7c76c0c9ee895877f5f043aa7be
Irene Shaw <irene.shaw@omnicell.com>
2019-10-29 17:13:28 -0400

    This will list your budgets and do nothing else ;-)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    ynab_api = YNAB::API.new(session[:auth]['credentials']['token'])
+
+    budget_response = ynab_api.budgets.get_budgets
+    @budgets = budget_response.data.budgets
   end
 end
