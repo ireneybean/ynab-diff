29f472623fc7bdeb80719b1e54e8f4874c85afb7
Irene Shaw <irene.shaw@omnicell.com>
2019-10-29 17:23:02 -0400

    add todo's

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
 end
+
+# TODO: before handler to assure authentication
+# TODO: Tests
+# TODO: Bootstrap/Tailwind?
+# TODO: make the call on whether to do a SPA style thing or a server-side style thing
+# TODO: Drill down to account in question (budgets controller, accounts controller)
+# TODO: Load CSV
+# TODO: package omniauth-ynab as gem or figure out why github one is failing
+# TODO: Rubocop
+#
