856a00756e82f14d64cc560be76bc5e650bfecf8
Irene Shaw <irene.shaw@omnicell.com>
2020-01-20 12:14:26 -0500

    Update readme

--- app/controllers/home_controller.rb
+++
@@ -1,8 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    ynab_api = YNAB::API.new(session[:auth]['credentials']['token'])
-
-    budget_response = ynab_api.budgets.get_budgets
-    @budgets = budget_response.data.budgets
-  end
-end
